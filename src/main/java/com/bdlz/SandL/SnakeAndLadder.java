e9f43a7e23679f38604f729583938cd127a8b299
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 17:11:27 +0530

    Added: Die Roll

--- src/main/java/com/bdlz/SandL/SnakeAndLadder.java
+++ src/main/java/com/bdlz/SandL/SnakeAndLadder.java
@@ -1,12 +1,15 @@
 package com.bdlz.SandL;
 
 public class SnakeAndLadder {
-    public static final int WINNING_POSITION = 100;
-    static int diceCount = 0;
+//    public static final int WINNING_POSITION = 100;
+//    static int diceCount = 0;
     public static void main(String[] args) {
-        int playerPosition = 0;
-        while (playerPosition < WINNING_POSITION) {
-            ++diceCount;
-        }
+//        int playerPosition = 0;
+//        while (playerPosition < WINNING_POSITION) {
+//            ++diceCount;
+//        }
+            int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            System.out.println("diceNumber" + diceNumber);
+
     }
 }
