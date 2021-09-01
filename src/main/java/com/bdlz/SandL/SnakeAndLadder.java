df9303a8657d0997b6113ca719358c30fa479fd9
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 17:44:53 +0530

    Added: If Player Moves below 0

--- src/main/java/com/bdlz/SandL/SnakeAndLadder.java
+++ src/main/java/com/bdlz/SandL/SnakeAndLadder.java
@@ -4,12 +4,12 @@ public class SnakeAndLadder {
     public static final int IS_LADDER = 1;
     public static final int IS_SNAKE = 2;
     public static final int WINNING_POSITION = 100;
+    public static int playerPosition = 0;
 
     static int diceCount = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder project");
-        int playerPosition = 0;
         while (playerPosition < WINNING_POSITION) {
             ++diceCount;
             playerPosition = getPosition(playerPosition);
@@ -22,18 +22,22 @@ public class SnakeAndLadder {
             System.out.println("It's Snake");
 
             if (position - diceNumber > 0) {
-                position -= diceNumber;
+                position = position - diceNumber;
+            } else if (position < 0) {
+                position = playerPosition;
+                System.out.println("Player moves below zero and starts from Zero");
             }
         } else if (option == IS_LADDER) {
             System.out.println("It's Ladder ");
 
             if (position + diceNumber <= 100) {
-                position += diceNumber;
+                position = position + diceNumber;
             }
-        } else {
+        } else{
             System.out.println("No play");
         }
-
+        System.out.println("dice count " + diceCount);
+        System.out.println("playerPosition " + position);
         return position;
     }
 }
