e256749737968c0335c8bc2b4afceee3de107edc
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 17:01:20 +0530

    Added: Player initial Poition

--- src/main/java/com/bdlz/SandL/SnakeAndLadder.java
+++ src/main/java/com/bdlz/SandL/SnakeAndLadder.java
@@ -1,7 +1,12 @@
 package com.bdlz.SandL;
 
 public class SnakeAndLadder {
+    public static final int WINNING_POSITION = 100;
+    static int diceCount = 0;
     public static void main(String[] args) {
-        System.out.println("Welcome to snake and ladder program");
+        int playerPosition = 0;
+        while (playerPosition < WINNING_POSITION) {
+            ++diceCount;
+        }
     }
 }
