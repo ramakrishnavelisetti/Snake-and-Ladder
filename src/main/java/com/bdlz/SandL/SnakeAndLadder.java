ec55d1b5120aace7d637936804997558070e7e99
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 18:20:45 +0530

    Added: Two Players

--- src/main/java/com/bdlz/SandL/SnakeAndLadder.java
+++ src/main/java/com/bdlz/SandL/SnakeAndLadder.java
@@ -4,44 +4,64 @@ public class SnakeAndLadder {
     public static final int IS_LADDER = 1;
     public static final int IS_SNAKE = 2;
     public static final int WINNING_POSITION = 100;
-    public static int playerPosition = 0;
 
     static int diceCount = 0;
+    static String turn = "player1";
 
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder project");
-        while (playerPosition < WINNING_POSITION) {
+        int player1Position = 0;
+        int player2Position = 0;
+        int player1DiceCount = 0;
+        int player2DiceCount = 0;
+        while (player1Position < WINNING_POSITION && player2Position < WINNING_POSITION) {
             ++diceCount;
-            playerPosition = getPosition(playerPosition);
+            if (turn.equals("player1")) {
+                turn = "player2";
+                ++player1DiceCount;
+                player1Position = getPosition(player1Position);
+            } else {
+                ++player2DiceCount;
+                turn = "player1";
+                player2Position = getPosition(player2Position);
+            }
         }
+
+        if (player1Position == WINNING_POSITION) {
+            System.out.println("Player1 win the game");
+            System.out.println("dice count " + player1DiceCount);
+        } else {
+            System.out.println("Player2 win the game");
+            System.out.println("dice count " + player2DiceCount);
+        }
+        System.out.println("Total dice count " + diceCount);
+
+        System.out.println("player1Position " + player1Position);
+        System.out.println("player2Position " + player2Position);
+
     }
-    public static int getPosition(int position) {
+
+    private static int getPosition(int position) {
         int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
         int option = (int) (Math.floor(Math.random() * 10) % 3);
         if (option == IS_SNAKE) {
-            System.out.println("It's Snake");
-
-            if (position - diceNumber > playerPosition) {
-                position = position - diceNumber;
-            } else if (position < playerPosition) {
-                position = playerPosition;
-                System.out.println("Player moves below zero and starts from Zero");
+            System.out.println("Snake for the player");
+            if (position - diceNumber > 0) {
+                position -= diceNumber;
             }
         } else if (option == IS_LADDER) {
-            System.out.println("It's Ladder ");
-
-            if (position + diceNumber <= WINNING_POSITION) {
-                position = position + diceNumber;
-            }else if (position > WINNING_POSITION) {
-                position = WINNING_POSITION;
-                System.out.println("Player moves above 100 and stoped");
+            System.out.println("Ladder for the player");
+            if (turn.equals("player1")) {
+                turn = "player1";
+            } else if (turn.equals("player2")) {
+                turn = "player2";
             }
-        } else{
-            System.out.println("No play");
+            if (position + diceNumber <= 100) {
+                position += diceNumber;
+            }
+        } else {
+            System.out.println("No play for the player");
         }
-        System.out.println("dice count " + diceCount);
-        System.out.println("playerPosition " + position);
         return position;
     }
 }
-
